d9435efdff6bf423d8e650f81d5a4a899cb15b30
mathilde.warstad <mathildewaerstad@gmail.com>
2020-03-01 22:45:43 +0100

    Include /build

---
+++ build/precache-manifest.d44ec7e3a7cf9652fd6f6fd1809525b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5afa931de5d542b69065ac5923b31eca",
+    "url": "/camp-hortenfestival/index.html"
+  },
+  {
+    "revision": "5d7806ab490da8586d3e",
+    "url": "/camp-hortenfestival/static/css/main.1d222d50.chunk.css"
+  },
+  {
+    "revision": "6ae5b90df44ff0fa63f0",
+    "url": "/camp-hortenfestival/static/js/2.c341940d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/camp-hortenfestival/static/js/2.c341940d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d7806ab490da8586d3e",
+    "url": "/camp-hortenfestival/static/js/main.eea93acb.chunk.js"
+  },
+  {
+    "revision": "785806add72f32c0bf76",
+    "url": "/camp-hortenfestival/static/js/runtime-main.26990bfc.js"
+  },
+  {
+    "revision": "fb322a1e732d60a5a8877c14f81bccd1",
+    "url": "/camp-hortenfestival/static/media/matta.fb322a1e.png"
+  }
+]);
