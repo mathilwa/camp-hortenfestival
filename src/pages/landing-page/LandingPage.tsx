f3a267500c62be0b60e229594ad923e1d762d379
mathilde.warstad <mathildewaerstad@gmail.com>
2020-03-05 23:48:47 +0100

    Legge til romoversikt

--- src/pages/landing-page/LandingPage.tsx
+++ src/pages/landing-page/LandingPage.tsx
@@ -5,9 +5,13 @@ import headingLogo from './icons/camp-hortenfestival.svg';
 import './landing-page.css';
 import confetti from 'canvas-confetti';
 import classNames from 'classnames';
+import andreEtasje from '../../components/romoversikt/icons/andre-etasje.svg';
+import forsteEtasje from '../../components/romoversikt/icons/forste-etasje.svg';
+import romfordelingLogo from '../../components/romoversikt/icons/romfordeling-logo.svg';
 
 const LandingPage = () => {
     const [mousePosition, setMousePosition] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
+
     const onMouseMove = (event: any) => {
         setMousePosition({ x: event.pageX, y: event.pageY });
     };
@@ -33,21 +37,30 @@ const LandingPage = () => {
 
     return (
         <div className="app" onMouseMove={onMouseMove} onClick={launceConfetti}>
-            <header className="appHeader">
-                <img src={matta} className="spinningUser" alt="logo" />
+            <header className="app-header">
+                <img src={matta} className="spinning-user" alt="logo" />
             </header>
 
-            <img
-                src={unicorn}
-                className={classNames('unicorn', isTochDevice && 'unicornMobile')}
-                alt="unicorn"
-                style={{ left: isTochDevice ? -130 : xPosition, top: isTochDevice ? 50 : yPosition }}
-            />
+            <div className="landing-page-top">
+                <img
+                    src={unicorn}
+                    className={classNames('unicorn', isTochDevice && 'unicorn-mobile')}
+                    alt="unicorn"
+                    style={{ left: isTochDevice ? -130 : xPosition, top: isTochDevice ? 50 : yPosition }}
+                />
 
-            <div className="headingLogoContainer">
-                <img src={headingLogo} alt="camp-hortenfestival-logo" />
+                <div className="heading-logo-container">
+                    <img src={headingLogo} alt="camp-hortenfestival-logo" />
+                </div>
+                <p className="festival-dates">18. - 21. juni</p>
             </div>
-            <p className="festivalDates">18. - 21. juni</p>
+            <div className="romoversikt">
+                <div className="romfordeling-heading">
+                    <img src={romfordelingLogo} alt="romfordeling-logo" />
+                </div>
+                <img className="etasje" src={forsteEtasje} alt="forste-etasje" />
+                <img className="etasje" src={andreEtasje} alt="andre-etasje" />
+            </div>
         </div>
     );
 };
